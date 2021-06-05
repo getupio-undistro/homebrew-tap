0de87feb733f3e99c00844c315e8d0545b56db2b
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-05 17:57:37 -0300

    Brew formula update for undistro version v0.27.2

--- Formula/undistro.rb
+++ Formula/undistro.rb
@@ -5,24 +5,24 @@
 class Undistro < Formula
   desc "UnDistro is a vanilla, non-opinionated, and open source Kubernetes distribution"
   homepage "https://undistro.io"
-  version "0.27.1"
+  version "0.27.2"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/getupio-undistro/undistro/releases/download/v0.27.1/undistro_0.27.1_darwin_amd64.tar.gz"
-    sha256 "bcaee9764fb475b1a280bc3be3da4851f4f4bfcb9ce39f7b694ef73bee6f4efd"
+    url "https://github.com/getupio-undistro/undistro/releases/download/v0.27.2/undistro_0.27.2_darwin_amd64.tar.gz"
+    sha256 "68039a0dc5c213b3178c43db7d5a2f51f6e7ce1f972b6111d29fccd1c1647cc7"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/getupio-undistro/undistro/releases/download/v0.27.1/undistro_0.27.1_darwin_arm64.tar.gz"
-    sha256 "9785e24f00b33f903177d20814b8bbb7b8056d661c350c587bd1500ebfbac40c"
+    url "https://github.com/getupio-undistro/undistro/releases/download/v0.27.2/undistro_0.27.2_darwin_arm64.tar.gz"
+    sha256 "21a31432c3e08aa5cdfb8b7a21b50f06a0af0564a81eda9c48ade329ed6ab423"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/getupio-undistro/undistro/releases/download/v0.27.1/undistro_0.27.1_linux_amd64.tar.gz"
-    sha256 "24a1606ebe080a7eb45fe4c09b5fbae3f954e2e6645613d4372f8a75575f11f9"
+    url "https://github.com/getupio-undistro/undistro/releases/download/v0.27.2/undistro_0.27.2_linux_amd64.tar.gz"
+    sha256 "ca910b691c23115d3fe49e4cb4137b0d60e2db518ec19f620728fe857dc8c957"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/getupio-undistro/undistro/releases/download/v0.27.1/undistro_0.27.1_linux_arm64.tar.gz"
-    sha256 "e0c998517c431b51cea41581006fcfb95b8826fd22d97776b6981c4d362be6a6"
+    url "https://github.com/getupio-undistro/undistro/releases/download/v0.27.2/undistro_0.27.2_linux_arm64.tar.gz"
+    sha256 "1250d2981a0648d38e6ce612d55dd0041c8bda632231702e2fa2dd022c4ae92e"
   end
 
   def install
