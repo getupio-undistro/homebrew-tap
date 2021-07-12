ba42ffa93afaf07f5d11c16b368bce163aeef3b7
goreleaserbot <goreleaser@carlosbecker.com>
2021-07-12 12:05:14 -0300

    Brew formula update for undistro version v0.31.2

--- Formula/undistro.rb
+++ Formula/undistro.rb
@@ -5,28 +5,28 @@
 class Undistro < Formula
   desc "UnDistro is a vanilla, non-opinionated, and open source Kubernetes distribution"
   homepage "https://undistro.io"
-  version "0.31.1"
+  version "0.31.2"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/getupio-undistro/undistro/releases/download/v0.31.1/undistro_0.31.1_darwin_amd64.tar.gz"
-      sha256 "c63804206258da53dc8d868a1cd87ec95ade01d70446a304b907ca1eed145673"
+      url "https://github.com/getupio-undistro/undistro/releases/download/v0.31.2/undistro_0.31.2_darwin_amd64.tar.gz"
+      sha256 "d4fed588444592245f7a00f029d3fdc23273a65ada70419262938bf67713fb81"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/getupio-undistro/undistro/releases/download/v0.31.1/undistro_0.31.1_darwin_arm64.tar.gz"
-      sha256 "c04f495370e56d4ccac66424e123e6962eea3891efa8f2135b0c6a1d905f4412"
+      url "https://github.com/getupio-undistro/undistro/releases/download/v0.31.2/undistro_0.31.2_darwin_arm64.tar.gz"
+      sha256 "0555a2670fa90e781281962e32b0efc6be459e9ad3e65172732dd5e5d4d04ab0"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/getupio-undistro/undistro/releases/download/v0.31.1/undistro_0.31.1_linux_amd64.tar.gz"
-      sha256 "b8ddb434e388f25d04525085f5904085d1790470645befc2ac28565554c8b663"
+      url "https://github.com/getupio-undistro/undistro/releases/download/v0.31.2/undistro_0.31.2_linux_amd64.tar.gz"
+      sha256 "e03e26a4f27107f5450a4d8fd34d4cf6b4353a1f91c73ea0c4a4ea8230280a63"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/getupio-undistro/undistro/releases/download/v0.31.1/undistro_0.31.1_linux_arm64.tar.gz"
-      sha256 "8f5489fb9c425f82ff802f9a7596162fbac7354540530631ca7e6b61641d7a94"
+      url "https://github.com/getupio-undistro/undistro/releases/download/v0.31.2/undistro_0.31.2_linux_arm64.tar.gz"
+      sha256 "0b0fbe1e28c00804c06b5b2d1992cbbc4b8e159c44fac3c275fe803325a44863"
     end
   end
 
